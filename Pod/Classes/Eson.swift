8ffbf875764a5ad3c59cfbfdca3f32a7819a4a66
Elliot <elliot.schrock@gmail.com>
2016-02-02 18:35:12 -0500

    refactor and clean up

--- Pod/Classes/Eson.swift
+++ Pod/Classes/Eson.swift
@@ -9,42 +9,41 @@
 import UIKit
 
 public class Eson: NSObject {
-    var transformers: [Transformer]? = [Transformer]()
+    public var serializers: [Serializer]! = [Serializer]()
+    public var deserializers: [Deserializer]! = [Deserializer]()
     
     public override init() {
-        self.transformers?.append(StringTransformer())
-        self.transformers?.append(IntTransformer())
+        self.serializers?.append(StringSerializer())
+        self.serializers?.append(IntSerializer())
+        self.serializers?.append(BoolSerializer())
     }
     
     public func toJsonDictionary(object: AnyObject) -> [String: AnyObject]? {
         var json = [String: AnyObject]()
         
-        let mirror = Mirror(reflecting: object)
-        
-        var children = [(label: String?, value: Any)]()
-        let mirrorChildrenCollection = AnyRandomAccessCollection(mirror.children)!
-        children += mirrorChildrenCollection
-        
-        var currentMirror = mirror
-        while let superclassChildren = currentMirror.superclassMirror()?.children {
-            let randomCollection = AnyRandomAccessCollection(superclassChildren)!
-            children += randomCollection
-            currentMirror = currentMirror.superclassMirror()!
+        var keyMap = [String:String]()
+        if object.dynamicType.respondsToSelector(Selector("esonPropertyNameToKeyMap")) {
+            keyMap = (object.dynamicType as! EsonKeyMapper.Type).esonPropertyNameToKeyMap()
         }
         
+        let children = childrenOfClass(object)
         for (optionalPropertyName, value) in children {
             let propertyName = optionalPropertyName!
             let propertyValue = value as? AnyObject
             
-            var isTransformed = false
-            for transformer in self.transformers! {
-                if propertyValue!.dynamicType === transformer.exampleValue().dynamicType {
-                    json[convertToSnakeCase(propertyName)] = transformer.objectForValue(value as? AnyObject)
-                    isTransformed = true
+            var isSerialized = false
+            for serializer in self.serializers {
+                if propertyValue!.dynamicType === serializer.exampleValue().dynamicType {
+                    var key = convertToSnakeCase(propertyName)
+                    if let mappedKey = keyMap[propertyName] {
+                        key = mappedKey
+                    }
+                    json[key] = serializer.objectForValue(value as? AnyObject)
+                    isSerialized = true
                     break;
                 }
             }
-            if !isTransformed {
+            if !isSerialized {
                 json[convertToSnakeCase(propertyName)] = toJsonDictionary((value as? AnyObject)!)
             }
         }
@@ -53,26 +52,91 @@ public class Eson: NSObject {
     
     public func fromJsonDictionary<T: NSObject>(jsonDictionary: [String: AnyObject]?, clazz: T.Type) -> T? {
         let object = clazz.init()
+        var keyMap = [String:String]()
+        if object.dynamicType.respondsToSelector(Selector("esonPropertyNameToKeyMap")) {
+            keyMap = (object.dynamicType as! EsonKeyMapper.Type).esonPropertyNameToKeyMap()
+        }
         if let json = jsonDictionary {
             for key: String in json.keys{
-                let camelCaseKey = convertToCamelCase(key)
-                if object.respondsToSelector(Selector(camelCaseKey)) {
-                    object.setValue(json[key], forKey: camelCaseKey)
+                var propertyKey = convertToCamelCase(key)
+                for propertyName in keyMap.keys {
+                    if keyMap[propertyName] == key {
+                        propertyKey = propertyName
+                    }
+                }
+                if object.respondsToSelector(Selector(propertyKey)) {
+                    let propertyClass = propertyTypeForName(object, name: propertyKey)!
+                    let propertyClassName = String(propertyClass)
+                    var isDeserialized = false
+                    for deserializer in self.deserializers! {
+                        let nameOfClass = deserializer.nameOfClass()
+                        if propertyClassName == nameOfClass {
+                            isDeserialized = true
+                            object.setValue(deserializer.valueForObject(json[key]!), forKey: propertyKey)
+                        }
+                    }
+                    if !isDeserialized {
+                        object.setValue(json[key], forKey: propertyKey)
+                    }
                 }
             }
         }
         return object
     }
     
-    public func toJsonArray(array: [AnyObject]?) -> [NSDictionary]? {
-        return nil
+    public func toJsonArray(array: [AnyObject]?) -> [[String: AnyObject]]? {
+        var result = [[String: AnyObject]]()
+        if let objectArray = array {
+            for object in objectArray {
+                result.append(toJsonDictionary(object)!)
+            }
+        }
+        return result
     }
     
-    public func fromJsonArray() -> [AnyObject]? {
-        return nil
+    public func fromJsonArray<T: NSObject>(array: [[String: AnyObject]]?, clazz: T.Type) -> [AnyObject]? {
+        var result = [AnyObject]()
+        if let jsonArray = array {
+            for json in jsonArray {
+                result.append(fromJsonDictionary(json, clazz: clazz)!)
+            }
+        }
+        return result
     }
     
     //MARK: non-public methods
+    
+    func childrenOfClass(object: AnyObject) -> [(label: String?, value: Any)] {
+        let mirror = Mirror(reflecting: object)
+        
+        var children = [(label: String?, value: Any)]()
+        let mirrorChildrenCollection = AnyRandomAccessCollection(mirror.children)!
+        children += mirrorChildrenCollection
+        
+        var currentMirror = mirror
+        while let superclassChildren = currentMirror.superclassMirror()?.children {
+            let randomCollection = AnyRandomAccessCollection(superclassChildren)
+            if let collection = randomCollection {
+                children += collection
+            }
+            currentMirror = currentMirror.superclassMirror()!
+        }
+        
+        return children
+    }
+    
+    func propertyTypeForName(object: NSObject, name: String) -> AnyClass? {
+        var propertyType: AnyClass?
+        let children = childrenOfClass(object)
+        
+        for (optionalPropertyName, value) in children {
+            let propertyName = optionalPropertyName!
+            if propertyName == name {
+                propertyType = (value as! AnyObject).dynamicType
+            }
+        }
+        return propertyType
+    }
     
     func convertToCamelCase(string: String) -> String {
         let stringArray = string.characters.split("_")
@@ -92,12 +156,21 @@ public class Eson: NSObject {
     }
 }
 
-public protocol Transformer {
+public protocol EsonKeyMapper {
+    static func esonPropertyNameToKeyMap() -> [String:String];
+}
+
+public protocol Deserializer {
+    func nameOfClass() -> String;
+    func valueForObject(object: AnyObject) -> AnyObject?;
+}
+
+public protocol Serializer {
     func objectForValue(value: AnyObject?) -> AnyObject?;
     func exampleValue() -> AnyObject;
 }
 
-public class IntTransformer: Transformer {
+public class IntSerializer: Serializer {
     public func objectForValue(value: AnyObject?) -> AnyObject? {
         return value
     }
@@ -106,20 +179,20 @@ public class IntTransformer: Transformer {
     }
 }
 
-public class StringTransformer: Transformer {
+public class BoolSerializer: Serializer {
+    public func objectForValue(value: AnyObject?) -> AnyObject? {
+        return value
+    }
+    public func exampleValue() -> AnyObject {
+        return Bool()
+    }
+}
+
+public class StringSerializer: Serializer {
     public func objectForValue(value: AnyObject?) -> AnyObject? {
         return value
     }
     public func exampleValue() -> AnyObject {
         return String()
-    }
-}
-
-public class ArrayTransformer: Transformer {
-    public func objectForValue(value: AnyObject?) -> AnyObject? {
-        return Eson().toJsonArray(value as? [AnyObject])
-    }
-    public func exampleValue() -> AnyObject {
-        return [AnyObject]()
     }
 }
